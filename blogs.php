33a70bc36050b54540747bfc6cb53c2d81753c56
LoganJohans <loganjohans@yahoo.com>
2024-10-02 18:35:33 -0500

    Changes for multiple image display on blogs

--- blogs.php
+++ blogs.php
@@ -54,10 +54,43 @@ $CURRENT_PAGE = "Blogs";
                     const imageContainer = document.createElement('div');
                     imageContainer.className = 'image-container';
 
+                    const leftArrow = document.createElement('span');
+                    leftArrow.className = 'left-arrow';
+                    leftArrow.innerHTML = '&#9664;'; 
+                    leftArrow.style.display = 'none';  
+
+                    const rightArrow = document.createElement('span');
+                    rightArrow.className = 'right-arrow';
+                    rightArrow.innerHTML = '&#9654;'; 
+                    rightArrow.style.display = 'none';  
+
                     const img = document.createElement('img');
 					img.src = `../photo_abcd_A/images/${post.blog_id}/${post.blog_id}.jpg`;
                     img.alt = 'Blog Image';
                     img.className = 'blog-photo';
+
+                    let currentImageIndex = 0;
+                    let images = [];
+                    
+                    if (post.images.length > 0) {
+                        img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[0]}`;
+                
+                        // Show arrows only if there's more than one image
+                        if (post.images.length > 1) {
+                            leftArrow.style.display = 'inline';  
+                            rightArrow.style.display = 'inline'; 
+                        }
+                    }
+
+                    leftArrow.addEventListener('click', () => {
+                        currentImageIndex = (currentImageIndex - 1 + post.images.length) % post.images.length;
+                        img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[currentImageIndex]}`;
+                    });
+
+                    rightArrow.addEventListener('click', () => {
+                        currentImageIndex = (currentImageIndex + 1) % post.images.length;
+                        img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[currentImageIndex]}`;
+                    });
 
                     const blogDescription = document.createElement('p');
                     blogDescription.className = 'blog-description';
@@ -69,7 +102,9 @@ $CURRENT_PAGE = "Blogs";
                     blogUserContainer.appendChild(userImage);
                     blogUserContainer.appendChild(username);
                     blogUserContainer.appendChild(userbutton);
+                    imageContainer.appendChild(leftArrow);
                     imageContainer.appendChild(img);
+                    imageContainer.appendChild(rightArrow);
                     blogContainer.appendChild(blogUserContainer);
                     blogContainer.appendChild(blogTitle);
                     blogContainer.appendChild(imageContainer);
--- get_blog_posts.php
+++ get_blog_posts.php
@@ -17,6 +17,24 @@ $blogPosts = array();
 // Show all the results in the database table
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        // Get the images for the blog post
+        $blog_id = $row['blog_id'];
+        $imageDir = "../photo_abcd_A/images/$blog_id/"; 
+        $images = array();
+
+        if (is_dir($imageDir)) {
+            // Scan for image files in the directory
+            $imageFiles = scandir($imageDir);
+            foreach ($imageFiles as $file) {
+                // Only include jpg and png files
+                if (preg_match('/\.(jpg|jpeg|png)$/i', $file)) {
+                    $images[] = $file; 
+                }
+            }
+        }
+
+        // Add images array to the blog post
+        $row['images'] = $images;
         $blogPosts[] = $row;
     }
 }
