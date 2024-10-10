f60804a2fb3bf8fce67fa39497aa1fd71c865828
cassmsand <121123151+cassmsand@users.noreply.github.com>
2024-10-10 11:26:20 -0500

    Match the footer to the header

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
-<div class="footer" id='main-footer'>
+<div class="footer" id='main-footer' style="background-image: url('../photo_abcd_A/images/banner.png'); background-size: cover; background-position: center;">
 	Alligator Group &copy; <?php print date("Y");?>
 </div>
