c92ae690076b097927bd65fd0db3b9d837875a6d
cassmsand <121123151+cassmsand@users.noreply.github.com>
2024-10-10 17:53:29 -0500

    Revert "Update blogs.phh, create count-files.php, edit photo file names"
    This reverts commit d5a53a9965509954b34c4df8c9e886068efd6aa2.

--- blogs.php
+++ blogs.php
@@ -37,6 +37,7 @@ $CURRENT_PAGE = "Blogs";
     </div>
 
     <div id="postsContainer"></div>
+
             <script>
                 // Function to fetch blog posts with sorting
                 const fetchBlogs = (title = '', startDate = '', endDate = '', sortOrder = 'asc') => {
@@ -84,42 +85,33 @@ $CURRENT_PAGE = "Blogs";
                                 rightArrow.style.display = 'none';
 
                                 const img = document.createElement('img');
-                                img.src = `../photo_abcd_A/images/${post.blog_id}/${post.blog_id}_0.jpg`;
+                                img.src = `../photo_abcd_A/images/${post.blog_id}/${post.blog_id}.jpg`;
                                 img.alt = 'Blog Image';
                                 img.className = 'blog-photo';
 
-                                fetch(`actions/count-files.php?blog_id=${post.blog_id}`)
-                                    .then(response => response.json())
-                                    .then(data => {
-                                        const fileCount = data.fileCount;
-                                        if (fileCount > 1) {
-                                            leftArrow.style.display = 'inline';
-                                            rightArrow.style.display = 'inline';
-                                        }
+                                let currentImageIndex = 0;
+                                let images = [];
 
-                                        currentImageIndex = 0;
+                                if (post.images.length > 0) {
+                                    img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[0]}`;
 
-                                        // Left arrow click event
-                                        leftArrow.addEventListener('click', () => {
-                                            currentImageIndex--;
-                                            if (currentImageIndex < 0) {
-                                                currentImageIndex = fileCount-1;
-                                            }
-                                            img.src = `../photo_abcd_A/images/${post.blog_id}/${post.blog_id}_${currentImageIndex}.jpg`;
-                                            
-                                        });
+                                    // Show arrows only if there's more than one image
+                                    if (post.images.length > 1) {
+                                        leftArrow.style.display = 'inline';
+                                        rightArrow.style.display = 'inline';
+                                    }
+                                }
 
-                                        // Right arrow click event
-                                        rightArrow.addEventListener('click', () => {
-                                            currentImageIndex++;
-                                            if (currentImageIndex == fileCount) {
-                                                currentImageIndex = 0; // Reset to zero if it reaches fileCount
-                                            }
-                                            img.src = `../photo_abcd_A/images/${post.blog_id}/${post.blog_id}_${currentImageIndex}.jpg`;
-                                        });
-                                    })
-                                    .catch(error => console.error('Error fetching file count:', error));
- 
+                                leftArrow.addEventListener('click', () => {
+                                    currentImageIndex = (currentImageIndex - 1 + post.images.length) % post.images.length;
+                                    img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[currentImageIndex]}`;
+                                });
+
+                                rightArrow.addEventListener('click', () => {
+                                    currentImageIndex = (currentImageIndex + 1) % post.images.length;
+                                    img.src = `../photo_abcd_A/images/${post.blog_id}/${post.images[currentImageIndex]}`;
+                                });
+
                                 const blogDescription = document.createElement('p');
                                 blogDescription.className = 'blog-description';
                                 blogDescription.textContent = post.description;
