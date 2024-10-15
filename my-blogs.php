40b176f17e57d79c806e28a81bc2f055140003fb
cassmsand <121123151+cassmsand@users.noreply.github.com>
2024-10-14 19:54:18 -0500

    Fix new blog modal

--- my-blogs.php
+++ my-blogs.php
@@ -6,16 +6,29 @@ $CURRENT_PAGE = "MyBlogs";
 <!DOCTYPE html>
 <html lang="en">
 
-	<title><?php print "My Blogs";?></title>
+<head>
+    <title><?php print "My Blogs"; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
 
-	<body>
-		<?php include("includes/top-bar.php");?>
-		<div class="container" id='main-body'>
-			<h2>My Blogs</h2>
-			<p>My Blogs information goes here.</p>
-			<p>My Blogs information goes here.</p>
-			<p>My Blogs information goes here.</p>
-        </div>
-		<?php include("includes/footer.php");?>
-	</body>
+<body>
+    <?php include("includes/top-bar.php"); ?>
+    <div class="container" id='main-body'>
+        <h2>My Blogs</h2>
+        <p>My Blogs information goes here.</p>
+
+        <!-- Button to open the modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newBlogModal">
+            Add New Blog
+        </button>
+
+        <!-- Include the modal from new-blog-modal.php -->
+        <?php include("includes/new-blog-modal.php"); ?>
+    </div>
+
+    <?php include("includes/footer.php"); ?>
+
+    <!-- Bootstrap JS Bundle (with Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
