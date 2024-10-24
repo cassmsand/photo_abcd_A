f03362d3be8a77ceb937953f2458359cd778d022
jher5403 <jher5403@gmail.com>
2024-10-24 01:03:15 -0500

    changed sample names and images

--- about.php
+++ about.php
@@ -14,35 +14,41 @@ $CURRENT_PAGE = "About";
     <section>
         <div class="container" id='main-body'>
 
-            <div class="container about-bar" id="left">
-                <div class="container image-container">
-                    <img class='img-fluid' src="../photo_abcd_A/images/about/samp1.jpg" alt="team image">
-                </div>
-
-                <div class="container">
-                    <div class="about-header">
-                        <h1 id="left">Alice Johnson</h1>
-                    </div>
-
-                    <div class="container about-bar">
-                        <p>
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                            unknown
-                            printer took a galley of type and scrambled it to make a type specimen book. It has survived
-                            not only five centuries, but also the leap into electronic typesetting, remaining
-                            essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                            containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                            Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
+            <div class="container about-bar row align-items-start" id="left">
+                <div class="col-3">
+                    <div class="container image-container">
+                        <img class='img-fluid' src="../photo_abcd_A/images/about/samp1.jpg" alt="team image">
                     </div>
                 </div>
+                
+                
+                <div class="col-9">
+                    <div class="container row">
+                        <div class="about-header">
+                            <h1 id="left">Alice Johnson</h1>
+                        </div>
+
+                        <div class="container about-bar">
+                            <p>
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                unknown
+                                printer took a galley of type and scrambled it to make a type specimen book. It has survived
+                                not only five centuries, but also the leap into electronic typesetting, remaining
+                                essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                                containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                                Aldus PageMaker including versions of Lorem Ipsum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
 
             <div class="container about-bar" id="right">
                 <div class="container">
 
                     <div class="about-header">
-                        <h1>Uvuvwevwevwe Onyenenyevwevwe Ugbwemubwem</h1>
+                        <h1>Arnold Noble</h1>
                     </div>
                     <div class="container about-bar">
                         <p>
@@ -67,7 +73,7 @@ $CURRENT_PAGE = "About";
                 </div>
                 <div class="container">
                     <div class="about-header">
-                        <h1>Phillip Philips</h1>
+                        <h1>Issac Conner</h1>
                     </div>
                     <div class="container about-bar">
                         <p>
@@ -86,7 +92,7 @@ $CURRENT_PAGE = "About";
             <div class="container about-bar" id="right">
                 <div class="container">
                     <div class="about-header">
-                        <h1>Michaelangelo</h1>
+                        <h1>Bridget Everett</h1>
                     </div>
                     <div class="container about-bar">
                         <p>
@@ -112,7 +118,7 @@ $CURRENT_PAGE = "About";
 
                 <div class="container">
                     <div class="about-header">
-                        <h1>Bob Dylan</h1>
+                        <h1>Darryl Mathews</h1>
                     </div>
                     <div class="container about-bar">
                         <p>
