f71b493815df675c63901b0a5b5113e9e4e95e06
jher5403 <jher5403@gmail.com>
2024-11-14 00:19:46 -0600

    Added Web Icon

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -9,6 +9,7 @@
         <meta name="keywords" content="" /> 
     <?php } ?>
 
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/home.css">
 </head>
