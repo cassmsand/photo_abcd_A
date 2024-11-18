6be22ed579515292cbafd8d997de2b8c23e9b18d
cassmsand <121123151+cassmsand@users.noreply.github.com>
2024-11-18 13:23:16 -0600

    Add initial setting contents

---
+++ includes/user-settings.php
@@ -0,0 +1,35 @@
+<body>
+    <div class="profile-section">
+        <div class="make-row">
+            <div class="profile-contents">
+                <div class="section-contents">
+                    <h5> Profile Photo: </h5>
+                    <div class="settings-profile-photo">
+                        <img src="<?=$userImg?>" alt="userImage">
+                    </div>
+                </div>
+                <button class="settings-button">Edit Profile Photo</button>
+            </div>
+            <div class="make-column">
+                <div class="profile-contents">
+                    <div class="section-contents">
+                        <h5> My First Name: </h5>
+                        <h6> FIRST NAME <h6>
+                    </div>
+                    <button class="settings-button">Edit First Name</button>
+                </div>
+                <div class="profile-contents">
+                    <div class="section-contents">
+                        <h5> My Last Name: </h5>
+                        <h6> LAST NAME <h6>
+                    </div>
+                    <button class="settings-button">Edit Last Name</button>
+                </div>
+            </div>
+        </div>
+        <div class="profile-password">
+            <h5> Password: </h5>
+            <button class="settings-button">Change Password</button>
+        </div>
+    </div>
+</body>
---
+++ settings.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include_once("includes/db-conn.php");
+$CURRENT_PAGE = "Settings";
+
+if (isset($_SESSION['current_user_email'])) {
+    $userImgDir = "images/users/".$_SESSION['current_user_email'];
+    $userImg = @scandir($userImgDir);
+    if ($userImg != false) {
+        $userImg = $userImgDir."/".array_values(array_diff($userImg, array('..', '.')))[0];
+    } else {
+        $userImg = "images/blankicon.jpg";
+    }
+    $widget_name = $_SESSION['current_user_first_name'];
+
+} else {
+    $userImg = "images/blankicon.jpg";
+    $widget_name = "Guest";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include("includes/head-tag-contents.php"); ?>
+        <link rel="stylesheet" href="css/settings.css">
+    </head>
+
+    <body>
+        <?php include("includes/top-bar.php");?>
+        <h2>Settings</h2>
+        <?php include("includes/user-settings.php");?>
+        <?php include("includes/footer.php");?>
+    </body>
+</html>
