ce236aca51188bb42590ce3c0e7e190f3a9e96d3
cassmsand <121123151+cassmsand@users.noreply.github.com>
2024-12-27 14:36:51 -0600

    Update login.php

--- actions/login.php
+++ actions/login.php
@@ -31,7 +31,7 @@ if (isset($_GET['email']) && isset($_GET['password'])) {
 
             if ($isLocalServer) {
                 // If on a local server, use the relative path to the default blank icon
-                $_SESSION['user_img'] = "/photo_abcd_A/images/blankicon.jpg";
+                $_SESSION['user_img'] = "/images/blankicon.jpg";
             }
             
             else {
